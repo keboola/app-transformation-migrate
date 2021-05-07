b1527be502a37d31c76cd781c4cb200822fe314f
Ondra Jodas <ondrej.jodas@keboola.com>
2021-05-07 07:33:23 +0200

    add missing transformation backends

--- src/Configuration/Config.php
+++ src/Configuration/Config.php
@@ -14,9 +14,17 @@ class Config extends BaseConfig
 
     private const PYTHON_COMPONENT_ID = 'keboola.python-transformation-v2';
 
+    private const REDSHIFT_COMPONENT_ID = 'keboola.redshift-transformation';
+
+    private const R_COMPONENT_ID = 'keboola.r-transformation-v2';
+
     public const TRANSFORMATION_TYPE_SNOWFLAKE = 'snowflake-simple';
 
+    public const TRANSFORMATION_TYPE_REDSHIFT = 'redshift-simple';
+
     public const TRANSFORMATION_TYPE_PYTHON = 'docker-python';
+
+    public const TRANSFORMATION_TYPE_R = 'docker-r';
 
     public static function getKnownBackends(): array
     {
@@ -33,6 +41,10 @@ class Config extends BaseConfig
                 return self::SNOWFLAKE_COMPONENT_ID;
             case Config::TRANSFORMATION_TYPE_PYTHON:
                 return self::PYTHON_COMPONENT_ID;
+            case Config::TRANSFORMATION_TYPE_R:
+                return self::R_COMPONENT_ID;
+            case Config::TRANSFORMATION_TYPE_REDSHIFT:
+                return self::REDSHIFT_COMPONENT_ID;
             default:
                 throw new InvalidConfigurationException(
                     sprintf('Unknown backend type "%s"', $transformationTypeKey)
