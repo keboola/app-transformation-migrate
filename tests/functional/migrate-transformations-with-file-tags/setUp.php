7b73080e6d97e0e729804c9b546266a59e1f4307
Ondra Jodas <ondrej.jodas@keboola.com>
2021-06-14 14:41:39 +0200

    failing test

---
+++ tests/functional/migrate-transformations-with-file-tags/setUp.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Keboola\TransformationMigrate\FunctionalTests\DatadirTest;
+use Keboola\TransformationMigrate\FunctionalTests\TestManager;
+
+return function (DatadirTest $test): void {
+    $manager = new TestManager($test->getComponentsClient());
+    $configuration = $manager->createBucket();
+
+    $manager->createTransformation(
+        $configuration,
+        'python row',
+        [
+            'backend' => 'docker',
+            'type' => 'python',
+            'phase' => 1,
+            'queries' => [
+                'print(1)',
+            ],
+            'tags' => [
+                'fileTag1',
+                'fileTag2',
+                'fileTag3',
+                'fileTag4',
+            ],
+        ],
+    );
+
+    $manager->createTransformation(
+        $configuration,
+        'python row 2',
+        [
+            'backend' => 'docker',
+            'type' => 'python',
+            'phase' => 1,
+            'queries' => [
+                'print(2)',
+            ],
+            'tags' => [
+                'fileTag2',
+                'fileTag3',
+            ],
+        ],
+    );
+
+    putenv('TRANSFORMATION_BUCKET_ID=' . $configuration->getConfigurationId());
+};
