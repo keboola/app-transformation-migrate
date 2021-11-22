7c487b72691eccf1392340a7fd529f8ed797cee5
Ondra Jodas <ondrej.jodas@keboola.com>
2021-11-22 10:28:39 +0100

    test for non-master token

---
+++ tests/functional/migrate-non-master-token/processEnv.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'KBC_TOKEN' => getenv('KBC_NON_MASTER_TOKEN'),
+];
---
+++ tests/functional/migrate-non-master-token/tearDown.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Keboola\StorageApi\Options\Components\ListConfigurationMetadataOptions;
+use Keboola\TransformationMigrate\FunctionalTests\DatadirTest;
+use Keboola\TransformationMigrate\FunctionalTests\TestManager;
+use PHPUnit\Framework\Assert;
+
+return function (DatadirTest $test): string {
+    $manager = new TestManager($test->getComponentsClient());
+
+    $result = [];
+    foreach ($test->getOutput() as $item) {
+        $transformation = $manager->getTransformationV2($item);
+        $result[] = $transformation['configuration'];
+
+        $oldTransformation = $manager->getTransformation($test->getTransformationBucketId());
+
+        $configurationMetadataOptions = new ListConfigurationMetadataOptions();
+        $configurationMetadataOptions
+            ->setComponentId($item['componentId'])
+            ->setConfigurationId($item['id'])
+        ;
+
+        Assert::assertArrayHasKey('configuration', $oldTransformation);
+        Assert::assertArrayHasKey('migrated', $oldTransformation['configuration']);
+        Assert::assertTrue($oldTransformation['configuration']['migrated']);
+        Assert::assertEquals('Mark as migrated', $oldTransformation['currentVersion']['changeDescription']);
+
+        $manager->removeTransformationV2($item);
+    }
+
+    return (string) json_encode($result, JSON_PRETTY_PRINT);
+};
